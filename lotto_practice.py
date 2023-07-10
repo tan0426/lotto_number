c56e4bd6dfa798a239a1ac33ed0d0be699e4a8ac
TanJung.Kim <106587677+tan0426@users.noreply.github.com>
2023-07-10 14:11:22 +0900

    Update lotto_practice.py

--- lotto_practice.py
+++ lotto_practice.py
@@ -16,6 +16,8 @@ lotto_table = {}
 i = 0
 j = 0
 
+pick_times = 5 #몇줄 뽑을건지
+
 #num_list 리스트 생성
 for i in range(1, 46, 1):
     num_list.append(i)
@@ -42,18 +44,27 @@ for i in range(0, lotto_excel_len, 1):
         number = lotto_excel_list[i][j]
         lotto_table[number] = lotto_table[number] + 1
 
+#뽑힌 횟수를 반대로 입력
+for i in range(1, 46, 1):
+    lotto_table[i] = lotto_excel_len - lotto_table[i]
+
+
 #뽑힌 횟수에 따라 가중치 입력
 for i in range(1, 46, 1):
     weights_list[i-1] = lotto_table[i]
 
+weights_list = [x+1 for x in weights_list] #0값 방지
+
 #중복 없이 6개 연달아 뽑기
-for i in range(0, 6, 1):
-    random_num_one = random.choices(range(1,46), weights= weights_list)[0]
-    while random_num_one in lotto_number_list:
+for j in range(0, pick_times, 1):
+    for i in range(0, 6, 1):
         random_num_one = random.choices(range(1,46), weights= weights_list)[0]
-    
-    lotto_number_list[i] = random_num_one
+        while random_num_one in lotto_number_list:
+            random_num_one = random.choices(range(1,46), weights= weights_list)[0]
+        
+        lotto_number_list[i] = random_num_one
 
-lotto_number_list.sort() #정렬
+    lotto_number_list.sort() #정렬
 
-print(lotto_number_list)
+    print(lotto_number_list)
+    j = j + 1
